edf83c697f9021591fa87d6629d3546d7c19fb19
Daniel Nevius <dmnevius@ncsu.edu>
2024-02-27 17:55:44 -0500

    Fixed bug in main

--- FacesToCaption.lrplugin/main.lua
+++ FacesToCaption.lrplugin/main.lua
@@ -1,6 +1,7 @@
 local LrTasks = import("LrTasks")
 local LrApplication = import("LrApplication")
 local LrLogger = import("LrLogger")
+local LrDialogs = import("LrDialogs")
 local FacesToCaption = import("./FacesToCaption")
 
 local logger = LrLogger("FacesToCaptionPlugin")
@@ -9,7 +10,9 @@ logger:enable("print")
 LrTasks.startAsyncTask(function ()
     local catalog = LrApplication.activeCatalog()
     local photo = catalog:getTargetPhoto()
-    if photo != nil then
+    if photo == nil then
+        LrDialogs.message("Faces to Caption", "Please select a photo before running Faces to Caption.")
+    else
         catalog:withWriteAccessDo("Write Faces to Caption", function () 
             FacesToCaption(photo.getFormattedMetadata('path'), logger.quickf('info'))
         end)
